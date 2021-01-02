e9cc68d7b5e9e1d111331e6c56568bf278749354
aboryka-bpol <aleksandra.boryka@bpol.net>
2021-01-02 13:50:10 +0100

    Update planets interface

--- src/planets/interfaces/planet.interface.ts
+++ src/planets/interfaces/planet.interface.ts
@@ -1,3 +1,13 @@
 export interface IPlanet {
     name?: string;
+    rotationPeriod?: number;
+    orbitalPeriod?: number;
+    diameter?: number;
+    climate?: string;
+    gravity?: string;
+    terrain?: string;
+    surfaceWater?: number;
+    population?: number;
+    films?: string[];
+    residents?: string[];
 }
