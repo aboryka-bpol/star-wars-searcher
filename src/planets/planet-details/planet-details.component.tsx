e9203183d6bc5dfa414d9b8a4d1d9187e4f503a6
aboryka-bpol <aleksandra.boryka@bpol.net>
2021-01-02 13:50:30 +0100

    Apply logic to planet detail component

--- src/planets/planet-details/planet-details.component.tsx
+++ src/planets/planet-details/planet-details.component.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { IPlanet } from '../interfaces/planet.interface';
 
-const PlanetDetails = (props: any) => {
+const PlanetDetails = (props: IPlanet) => {
+  const {name, rotationPeriod, orbitalPeriod, diameter, climate, gravity, terrain, surfaceWater, population, films, residents} = props;
+
+  const renderedFilms = films && films.map((film: string, index: number) => {
+    return (
+      <div key={index}>{film}</div>
+    )
+  });
+
+  const renderedResidents = residents && residents.map((resident: string, index: number) => {
+    return (
+      <div key={index}>{resident}</div>
+    )
+  })
+
   return (
     <div>
-     PlanetDetails
+      <div>Planet Name: {name}</div>
+      <div>Rotation Period: {rotationPeriod}</div>
+      <div>Orbital Period: {orbitalPeriod}</div>
+      <div>Diameter: {diameter}</div>
+      <div>Climate: {climate}</div>
+      <div>Gravity: {gravity}</div>
+      <div>Terrain: {terrain}</div>
+      <div>Surface Water: {surfaceWater}</div>
+      <div>Population: {population}</div>
+      <div>Film Resources:
+        <div>
+          {renderedFilms}
+        </div>
+      </div>
+      <div>Resident Resources:
+        <div>
+          {renderedResidents}
+        </div>
+      </div>
     </div>
   );
 }
